0433a280c9b4c41a5bafc9b89cb3795d3bcf1ced
Xavier Berger <xavier.berger66@gmail.com>
2025-01-20 16:22:04 +0100

    authentication, player and postgres microservices

---
+++ backend/player/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Player
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+class PlayerInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = [
+            'id',           
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'avatar',
+            'champions',
+            'wins',
+            'losses',
+            'two_factor',
+            'state',        # Champ avec des choix
+        ]
+        read_only_fields = ['id', 'champions', 'wins', 'losses', 'state']
