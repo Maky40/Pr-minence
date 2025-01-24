edb06902c5c0c648821d5536e56df7988b0f2f6d
mrabat <mrabat@student.42perpignan.fr>
2025-01-24 15:33:40 +0100

    update 42button

--- frontend/components/42button.js
+++ frontend/components/42button.js
@@ -8,15 +8,16 @@ class Button42 extends Component {
 
   template() {
       return `
-          <button class="btn btn-success btn-lg px-5 py-3 rounded-pill shadow-sm" 
-                  onclick="window.location.href='https://localhost/authentication/intra/'">
-              <img src="/assets/42_Logo.svg" 
-                   alt="42 Logo" 
-                   height="30"
-                   class="me-2" 
-                   style="filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(90%) contrast(95%); transition: filter 0.3s ease">
-              ${this.text}
-          </button>`;
+        <button class="btn btn-success btn-lg px-5 py-3 rounded-pill shadow-sm d-flex align-items-center"
+            onclick="window.location.href='https://localhost/authentication/intra/'">
+        <img src="/assets/42_Logo.svg" 
+                alt="42 Logo" 
+                height="30"
+                class="me-2" 
+                style="filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(90%) contrast(95%); transition: filter 0.3s ease">
+        ${this.text}
+        </button>
+        `;
   }
 }
 
