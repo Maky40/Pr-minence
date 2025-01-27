812c2cae63a1fe0a6fb1f9ed8139e6d4bf374b51
mrabat <mrabat@student.42perpignan.fr>
2025-01-27 15:24:53 +0100

    refactor button component layout and clean up HTML structure in signup and connexion pages

--- frontend/components/42button.js
+++ frontend/components/42button.js
@@ -1,22 +1,30 @@
-import Component from '../utils/Component.js';
+import Component from "../utils/Component.js";
 
 class Button42 extends Component {
   constructor(text = "S'inscrire avec 42") {
-      super();
-      this.text = text;
+    super();
+    this.text = text;
   }
 
   template() {
-      return `
-        <button class="btn btn-success btn-lg px-5 py-3 rounded-pill shadow-sm d-flex align-items-center"
-            onclick="window.location.href='https://localhost/authentication/intra/'">
-        <img src="/assets/42_Logo.svg" 
-                alt="42 Logo" 
-                height="30"
-                class="me-2" 
-                style="filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(90%) contrast(95%); transition: filter 0.3s ease">
-        ${this.text}
-        </button>
+    return `
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-success px-5 py-3 rounded-pill shadow-sm align-items-center w-100"
+                                onclick="window.location.href='https://localhost/authentication/intra/'">
+                                <img src="/assets/42_Logo.svg" 
+                                    alt="42 Logo" 
+                                    height="30"
+                                    class="me-2" 
+                                    style="filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(90%) contrast(95%); transition: filter 0.3s ease">
+                                ${this.text}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         `;
   }
 }
