ad33e0b82974586254557b6673ff51a53ac61583
mrabat <mrabat@student.42perpignan.fr>
2025-02-21 15:45:26 +0100

    add env.js to connexion

--- frontend/env.js
+++ frontend/env.js
@@ -3,7 +3,7 @@ const ENV = {
   URL_AUTH_INTRA: "authentication/intra/",
   WS_URL_GAME: "wss://localhost/pong/ws/pong/",
   WS_URL_AUTH: "wss://localhost/authentication/ws/online/",
-  URL_AUTH_DJANGO: "/authentication/login/",
+  URL_AUTH_DJANGO: "authentication/login/",
   URL_AUTH_DJANGO_SIGNUP: "/authentication/signup/",
   URL_AUTH_DJANGO_LOGOUT: "/authentication/logout/",
   DEFAULT_AVATAR: "player/static/api/images/default_avatar.png",
